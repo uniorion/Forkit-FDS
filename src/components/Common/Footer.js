6f0e17c09dea9aa07e1ffd2168e6432e4f145db4
uniorion <uniorion@naver.com>
2016-12-10 23:52:55 +0900

    footer 마크업

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -1,1 +1,19 @@
-//Footer
+import React, {Component} from 'react';
+
+class Footer extends Component {
+  render () {
+    return (
+      <Footer>
+        <p>&copy; Copyright 2016 ForkIt</p>
+        <ul>
+          <li><a href="#">ForkIt 소개</a></li>
+          <li><a href="#">이용약관</a></li>
+          <li><a href="#">개인정보취급방침</a></li>
+          <li><a href="#">문의하기</a></li>
+        </ul>
+      </Footer>
+    )
+  }
+}
+
+export default Footer
