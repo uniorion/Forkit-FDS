c378cf0d10be19b564f92d99b69feec2548e4181
Jinho Yim <jinhoyim@outlook.com>
2016-12-22 03:41:10 +0900

    Rating 읽기전용 및 스타일 추가

--- src/components/Review/Rating.js
+++ src/components/Review/Rating.js
@@ -1,19 +1,44 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 
 class Rating extends Component
 {
+  constructor(props){
+    super(props);
+    this.state = {
+      rating: props.defaultValue
+    };
+  }
+
   render(){
+    const stars = [];
+    for(let i=1; i <= this.props.max; i++)
+    { 
+      stars.push(
+        <span
+          key={i}
+          className={ i <= this.state.rating ? 'rating-on' : null}
+        >&#9734;</span>
+      );
+    }
+
     return (
-      <div>
-        <span>&#9734;</span>
-        <span>&#9734;</span>
-        <span>&#9734;</span>
-        <span>&#9734;</span>
-        <span>&#9734;</span>
+      <div className={'rating ' + this.props.className}>
+        {stars}
         <input type="hidden" />
       </div>
     );
   }
 }
+
+Rating.propTypes = {
+  defaultValue: PropTypes.number,
+  readonly: PropTypes.bool,
+  max: PropTypes.number
+};
+
+Rating.defaultProps = {
+  defaultValue: 0,
+  max: 5
+};
 
 export default Rating;
