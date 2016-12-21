2e2cc117887fec2d6612535de97c77692adb864c
uniorion <uniorion@naver.com>
2016-12-21 20:10:23 +0900

    Map 컴포넌트 수정
    - react-google-map 사용

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,86 +1,74 @@
 import React, {Component} from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+// import { connect } from 'react-redux';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
-import * as MapActions from '../../actions/map';
+const GoogleMapComponent = withGoogleMap(props => (
+  <GoogleMap
+    ref={props.onMapMounted}
+    zoom={props.zoom}
+    center={props.center}
+  >
+   {props.items.map(item => (
+      <Marker
+        position={{ lat: item.latitude, lng: item.longitude }}
+        key={item.id}
+        title='Click to zoom'
+        onClick={props.onMarkerClick}
+        icon='https://mp-seoul-image-production-s3.mangoplate.com/web/resources/ikpswdksy8bnweeq.png'
+      />
+    ))}
+  </GoogleMap>
+));
 
-let markers = [];
-let map = null;;
-
-// Set center: LatLngBounds object
-let bounds = null;
-
-class Map extends Component
-{
-  componentDidMount(){
-    this.loadGoogleMapApi();
+const INITIAL_SETTINGS = {
+  position : {
+    lat: 37.5, 
+    lng: 127.0 
   }
-  
-  componentWillReceiveProps(nextProps){
-    if ( nextProps.mapApi ) { // true : api 스크립트 로드하여 body에 append 된 상태
-      this.initMap();
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if ( this.props.items.length > 0 ) {
-      this.attachMarkers(this.props.items);
-    }
-  }
+};
 
-  // 구글 맵 api 스크립트 로드
-  loadGoogleMapApi(){
-    this.props.actions.fetchGoogleMapApiIfNeeded();
-  }
+export default class Map extends Component {
 
-  initMap() {
-    bounds = new google.maps.LatLngBounds();
-    map = new google.maps.Map(document.getElementById('google-map'), {
-      zoom: 8,
-      center: {lat: 37.523307, lng: 127.03616}
-    });
-  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      zoom: 10,
+      center: INITIAL_SETTINGS.position,
+    };
 
-  attachMarkers(items) {
-    for (let i = 0; i < items.length; i++) {
-      this.addMarkerWithTimeout({lat: items[i].latitude, lng: items[i].longitude}, i * 200);
-      bounds.extend(new google.maps.LatLng({lat: items[i].latitude, lng: items[i].longitude}));
-    }
-    //  Fit these bounds to the map
-    map.fitBounds(bounds);
-
-    if ( items.length === 1 ) { 
-      map.setZoom(16);
-    }
+    this.handleMapMounted = this.handleMapMounted.bind(this);
+    this.handleMarkerClick = this.handleMarkerClick.bind(this);
   }
 
-  addMarkerWithTimeout(position) {
-    let marker = new google.maps.Marker({
-      position: position,
-      map: map,
-
-      // icon: '../images/icon-placeholder.svg'
-      icon: 'https://mp-seoul-image-production-s3.mangoplate.com/web/resources/ikpswdksy8bnweeq.png'
-    });
-    markers.push(marker);
+  handleMapMounted(map) {
+    this._map = map;
   }
 
-  render(){
+  handleMarkerClick() {
+
+  }
+
+  componentWillUnmount() {
+    
+  }
+
+  render() {
     return (
-      <article id="google-map" className={this.props.styleClassName}>
-      </article>
+      <GoogleMapComponent
+        containerElement={
+          <div className={this.props.containerClassName} />
+        }
+        mapElement={
+          <div className={this.props.mapClassName} />
+        }
+        items={this.props.items}
+
+        zoom={window.parseInt(this.props.zoom, 10)}
+        center={this.state.center}
+        onMapMounted={this.handleMapMounted}
+        onMarkerClick={this.handleMarkerClick}
+      />
     );
   }
 }
-
-const mapStateToProps = state => ({
-  isFetching: state.googleMap.isFetching,
-  mapApi: state.googleMap.mapApi
-});
-
-const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators({
-    fetchGoogleMapApiIfNeeded: MapActions.fetchGoogleMapApiIfNeeded,
-  }, dispatch)
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Map);
